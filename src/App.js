7cd2107e12ee316dc7dec820498976240048e9fe
MidgetDude <61662276+MidgetDude@users.noreply.github.com>
2021-11-21 16:44:54 +0200

    Fixed automobiliai

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import firebase from "./firebase";
 import { useState, useEffect } from "react";
@@ -67,17 +66,19 @@ function App() {
       <table>
         <tr>
           <th>Id</th>
-          <th>Kaina</th>
-          <th>Nuomos pabaiga</th>
-          <th>Uzsakovo id</th>
+          <th>kebulo tipas</th>
+          <th>kuro tipas</th>
+          <th>vietu skaicius</th>
+          <th>pavaru deze</th>
         </tr>
         {data &&
-          data.Sutartis.map((sutartis, index) => (
+          data.Automobilis.map((automobilis, index) => (
             <tr>
               <td>{index}</td>
-              <td>{sutartis.kaina}</td>
-              <td>{sutartis.nuomos_pabaiga}</td>
-              <td>{sutartis.uzsakovo_id}</td>
+              <td>{automobilis.kebulo_tipas}</td>
+              <td>{automobilis.kuro_tipas}</td>
+              <td>{automobilis.vietu_skaicius}</td>
+              <td>{automobilis.pavaru_deze}</td>
             </tr>
           ))}
       </table>
@@ -86,111 +87,3 @@ function App() {
 }
 
 export default App;
-
-// import logo from "./logo.svg";
-// import "./App.css";
-// import databases from "./firebase";
-// import { useState, useEffect } from "react";
-
-// function App() {
-//   const firstDatabase = databases[0].database().ref("/DB_1");
-//   const secondDatabase = databases[1].database().ref("/DB_3");
-//   const [firstDB, setFirstDB] = useState("");
-//   const [dataSecondDB, setDataSecondDB] = useState("");
-//   const [loading, setLoading] = useState("");
-//   const getData = () => {
-//     firstDatabase.on("value", function (snapshot) {
-//       setFirstDB(snapshot.val());
-//       setLoading(true);
-//     });
-//     secondDatabase.on("value", function (snapshot) {
-//       setDataSecondDB(snapshot.val());
-//       setLoading(true);
-//     });
-//   };
-//   useEffect(() => {
-//     getData();
-//   }, []);
-//   console.log(firstDB);
-//   return (
-//     <div className="App">
-//       <h1>Sutartis</h1>
-//       <table>
-//         <tr>
-//           <th>Id</th>
-//           <th>Kaina</th>
-//           <th>Nuomos pabaiga</th>
-//           <th>Uzsakovo id</th>
-//         </tr>
-//         {firstDB &&
-//           firstDB.Sutartis.map((sutartis, index) => (
-//             <tr>
-//               <td>{index}</td>
-//               <td>{sutartis.kaina}</td>
-//               <td>{sutartis.nuomos_pabaiga}</td>
-//               <td>{sutartis.uzsakovo_id}</td>
-//             </tr>
-//           ))}
-//       </table>
-
-//       <h1>Automobilis</h1>
-//       <table>
-//         <tr>
-//           <th>Id</th>
-//           <th>Kaina</th>
-//           <th>Nuomos pabaiga</th>
-//           <th>Uzsakovo id</th>
-//         </tr>
-//         {firstDB &&
-//           firstDB.Sutartis.map((sutartis, index) => (
-//             <tr>
-//               <td>{index}</td>
-//               <td>{sutartis.kaina}</td>
-//               <td>{sutartis.nuomos_pabaiga}</td>
-//               <td>{sutartis.uzsakovo_id}</td>
-//             </tr>
-//           ))}
-//       </table>
-
-//       <h1>Pristatymo vieta</h1>
-//       <table>
-//         <tr>
-//           <th>Id</th>
-//           <th>Kaina</th>
-//           <th>Nuomos pabaiga</th>
-//           <th>Uzsakovo id</th>
-//         </tr>
-//         {firstDB &&
-//           firstDB.Sutartis.map((sutartis, index) => (
-//             <tr>
-//               <td>{index}</td>
-//               <td>{sutartis.kaina}</td>
-//               <td>{sutartis.nuomos_pabaiga}</td>
-//               <td>{sutartis.uzsakovo_id}</td>
-//             </tr>
-//           ))}
-//       </table>
-
-//       <h1>Uzsakovas</h1>
-//       <table>
-//         <tr>
-//           <th>Id</th>
-//           <th>Kaina</th>
-//           <th>Nuomos pabaiga</th>
-//           <th>Uzsakovo id</th>
-//         </tr>
-//         {firstDB &&
-//           firstDB.Sutartis.map((sutartis, index) => (
-//             <tr>
-//               <td>{index}</td>
-//               <td>{sutartis.kaina}</td>
-//               <td>{sutartis.nuomos_pabaiga}</td>
-//               <td>{sutartis.uzsakovo_id}</td>
-//             </tr>
-//           ))}
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default App;
