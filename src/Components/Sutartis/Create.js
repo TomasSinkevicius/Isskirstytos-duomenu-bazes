c9dd4428b45bffa02746e7ed0a817f66215c1913
MidgetDude <61662276+MidgetDude@users.noreply.github.com>
2021-11-22 11:05:22 +0200

    Fixing add

--- src/Components/Sutartis/Create.js
+++ src/Components/Sutartis/Create.js
@@ -7,8 +7,10 @@ import { Button } from "../StyleFiles/Button.style";
 
 export default function Create() {
   const [sutartiesDetails, setSutartiesDetails] = useState({
+    data: "",
     kaina: "",
     nuomos_pabaiga: "",
+    nuomos_pradzia: "",
     uzsakovo_id: "",
   });
 
@@ -22,17 +24,37 @@ export default function Create() {
   const create = (e) => {
     e.preventDefault();
 
-    let formData = new FormData();
-    formData.append("kaina", sutartiesDetails.kaina);
-    formData.append("nuomos_pabaiga", sutartiesDetails.nuomos_pabaiga);
-    formData.append("uzsakovo_id", sutartiesDetails.uzsakovo_id);
+    let formData1 = new FormData();
+    formData1.append("kaina", sutartiesDetails.kaina);
+    formData1.append("nuomos_pabaiga", sutartiesDetails.nuomos_pabaiga);
+    formData1.append("uzsakovo_id", sutartiesDetails.uzsakovo_id);
 
-    const ref = firebase.db1.database().ref("DB_1/Sutartis");
+    let formData2 = new FormData();
+    formData2.append("data", sutartiesDetails.data);
+    formData2.append("kaina", sutartiesDetails.kaina);
+    formData2.append("nuomos_pabaiga", sutartiesDetails.nuomos_pabaiga);
+    formData2.append("nuomos_pradzia", sutartiesDetails.nuomos_pradzia);
 
-    console.log(sutartiesDetails);
-
-    ref.push(sutartiesDetails);
+    if (sutartiesDetails.kaina < 500) {
+      const ref1 = firebase.db1.database().ref("DB_1/Sutartis");
+      const ref2 = firebase.db1.database().ref("DB_2/Sutartis");
+      // ref1.push(formData1);
+      // ref2.push(formData2);
+      console.log(sutartiesDetails);
+    } else {
+      const ref1 = firebase.db2.database().ref("DB_3/Sutartis");
+      const ref2 = firebase.db2.database().ref("DB_4/Sutartis");
+      // ref1.push(formData1);
+      // ref2.push(formData2);
+      console.log(sutartiesDetails);
+    }
   };
+
+  let today = new Date();
+  let [date, setDate] = useState(
+    today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+  );
+
   return (
     <AppContainer>
       <Row>
@@ -49,8 +71,20 @@ export default function Create() {
           <Form>
             <GridDiv>
               <input
+                type="date"
+                name="data"
+                onChange={handleOnChange}
+                value = {date}
+              ></input>
+              <input
                 type="number"
                 name="kaina"
+                onChange={handleOnChange}
+                initialvalue=""
+              ></input>
+              <input
+                type="date"
+                name="nuomos_pradzia"
                 onChange={handleOnChange}
                 initialvalue=""
               ></input>
